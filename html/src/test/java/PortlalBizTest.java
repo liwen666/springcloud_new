d617f55a9ee77692ec56e15d8ef175e82c517fbb
custom <1316138287@qq.com>
2019-05-27 10:05:20 +0800

    springboot单元测试

---
+++ html/src/test/java/PortlalBizTest.java
@@ -0,0 +1,29 @@
+import com.tjhq.portal.data.biz.impl.PortalBiz;
+import com.tjhq.portal.data.entity.Portal;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class PortlalBizTest extends BaseTest {
+
+	@Autowired
+	private PortalBiz portalBiz;
+
+	@Test
+	public void testCreate() {
+		Portal portal = portalBiz.getEntityByJpql("from Portal b where b.id=?0", "thisisademoportalfortest");
+		if (portal == null) {
+			portal = new Portal();
+			portal.setId("thisisademoportalfortest");
+			portal.setTitle("门户测试页面");
+			portal.setFitMode("both");
+			portal.setWidth(1500);
+			portal.setHeight(1200);
+			portalBiz.createEntity(portal);
+		}
+	}
+
+	@Test
+	public void testDelete() {
+
+	}
+}
