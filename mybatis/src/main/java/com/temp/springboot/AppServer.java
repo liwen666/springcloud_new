c407757c2389a736a6a00edd0294e1bdb1f3945a
custom <1316138287@qq.com>
2019-06-11 16:29:52 +0800

    mybatis基础测试

--- mybatis/src/main/java/com/temp/springboot/AppServer.java
+++ mybatis/src/main/java/com/temp/springboot/AppServer.java
@@ -3,10 +3,15 @@ package com.temp.springboot;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.sql.DataSource;
+
 //@EnableFeignClients
 //@ServletComponentScan
 @SpringBootApplication
@@ -16,7 +21,13 @@ import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
 public class AppServer extends TomcatServletWebServerFactory {
 
     public static void main(String[] args) {
-        SpringApplication.run(AppServer.class, args);
+//        SpringApplication.run(AppServer.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(AppServer.class, args);
+        ConfigurableListableBeanFactory beanFactory = run.getBeanFactory();
+//        run.close();
+        DataSource bean = beanFactory.getBean(DataSource.class);
+        System.out.println(bean);
+
     }
 
 
