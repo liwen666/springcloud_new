d3b1248da417269cf2ef73bf03bd98b527ff91cf
custom <1316138287@qq.com>
2019-06-14 14:56:48 +0800

    自定义配置文件

--- final_lw/src/main/java/com/lw/common/config/AppCfg.java
+++ final_lw/src/main/java/com/lw/common/config/AppCfg.java
@@ -1,42 +1,41 @@
 package com.hq.bpmn.common.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * app封装类
- *  business:
+ * 为静态变量赋值
+ * business:
      app:
      mh: http://10.10.15.143:8003
      bb: http://10.10.15.143:8003
-     hqbpmn: http://lacalhost:9091
+     hqbpmn: http://127.0.0.1:9091
+     server-enable: true
+ *
+ *
+ * app封装类
  * @date 2019/6/12 14:37
  */
 
 @Configuration
-@ConditionalOnProperty(prefix = "business.server", name = "enable", matchIfMissing = false)
 @ConfigurationProperties(prefix = "business")
-@EnableConfigurationProperties(AppCfg.class)
 public class AppCfg {
 
+    public static boolean serverEnable =false;
     /**
      * 从配置文件中读取的app开头的数据
      * 注意：名称必须与配置文件中保持一致
      */
-    private Map<String, String> app = new HashMap<>();
-
-    public Map<String, String> getApp() {
-        return app;
-    }
+    public static Map<String, String> app = new HashMap<>();
 
     public void setApp(Map<String, String> app) {
-        this.app = app;
+        AppCfg.app = app;
     }
 
-
+    public void setServerEnable(boolean serverEnable) {
+        AppCfg.serverEnable = serverEnable;
+    }
 }
