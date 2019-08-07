67a5f1fb1d69f3c626f6bced145ff08f3a3349bb
liwen <1438131288@qq.com>
2019-08-07 20:23:40 +0800

    dubbo自定义策略配置

--- filesystem/src/main/java/vip/dcpay/filesystem/config/config/DcpayDubboMethodConfig.java
+++ filesystem/src/main/java/vip/dcpay/filesystem/config/config/DcpayDubboMethodConfig.java
@@ -5,6 +5,8 @@ import com.alibaba.dubbo.common.utils.Holder;
 import com.alibaba.dubbo.config.ConsumerConfig;
 import com.alibaba.dubbo.rpc.cluster.LoadBalance;
 import com.alibaba.fastjson.JSON;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 import java.lang.reflect.Field;
 import java.util.Map;
@@ -14,46 +16,47 @@ import java.util.Map;
  * date 2019/8/7  18:44
  * discribe 配置dubbo
  */
+@Configuration
+@ConfigurationProperties(prefix = "dcpay.dubbo")
 public class DcpayDubboMethodConfig extends ConsumerConfig {
     @Override
     public void setLoadbalance(String loadbalance) {
-//        ExtensionLoader.getExtensionLoader(type).hasExtension(value)
-        boolean leastactive = ExtensionLoader.getExtensionLoader(LoadBalance.class).hasExtension("dcpaybalance");
-        System.out.println(leastactive);
-        ExtensionLoader<LoadBalance> extensionLoader = ExtensionLoader.getExtensionLoader(LoadBalance.class);
-        Field[] declaredFields = extensionLoader.getClass().getDeclaredFields();
-        for (Field f : declaredFields) {
-            if (f.getName().equals("cachedNames")) {
-                f.setAccessible(true);
-                try {
-                    Object o = f.get(extensionLoader);
-                    ((Map) o).put("class vip.dcpay.commission.config.DcpayBalance", "dcpaybalance");
-                    f.set(extensionLoader, o);
-                    System.out.println(JSON.toJSONString(o));
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
+        if("dcpaybalance".equals(loadbalance)){
+            System.out.println("===============================这一部分是防止懒加载无法替换balance的值==========================================================");
+            ExtensionLoader.getExtensionLoader(LoadBalance.class).hasExtension("dcpaybalance");
+            System.out.println("=========================================================================================");
+            ExtensionLoader<LoadBalance> extensionLoader = ExtensionLoader.getExtensionLoader(LoadBalance.class);
+            Field[] declaredFields = extensionLoader.getClass().getDeclaredFields();
+            for (Field f : declaredFields) {
+                if (f.getName().equals("cachedNames")) {
+                    f.setAccessible(true);
+                    try {
+                        Object o = f.get(extensionLoader);
+                        ((Map) o).put("class vip.dcpay.commission.config.DcpayBalance", "dcpaybalance");
+                        f.set(extensionLoader, o);
+                        System.out.println(JSON.toJSONString(o));
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                    f.setAccessible(false);
                 }
-                f.setAccessible(false);
-            }
-            if (f.getName().equals("cachedClasses")) {
-                f.setAccessible(true);
-                try {
-                    Object o = f.get(extensionLoader);
+                if (f.getName().equals("cachedClasses")) {
+                    f.setAccessible(true);
+                    try {
+                        Object o = f.get(extensionLoader);
 
-                    ((Map) ((Holder) o).get()).put("dcpaybalance", Class.forName("vip.dcpay.commission.config.DcpayBalance"));
-                    f.set(extensionLoader, o);
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
+                        ((Map) ((Holder) o).get()).put("dcpaybalance", Class.forName("vip.dcpay.commission.config.DcpayBalance"));
+                        f.set(extensionLoader, o);
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    f.setAccessible(false);
                 }
-                f.setAccessible(false);
             }
         }
-        boolean leastactive2 = ExtensionLoader.getExtensionLoader(LoadBalance.class).hasExtension("dcpaybalance");
-        System.out.println(leastactive2);
 
-        System.out.println("=========================================================================================");
         super.setLoadbalance(loadbalance);
     }
 }
